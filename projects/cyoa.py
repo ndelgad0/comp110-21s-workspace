c6b8c29703b69da87e61202aaaf8ef6bb7cd5e10
Nicole Delgado <ndelgado@live.unc.edu>
2021-02-23 19:49:40 -0500

    "you picked rachael" projects.cyoa

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -2,11 +2,22 @@
 
 def main() -> None:
     player: str = str(greet())
-    
+    choice1: str = str(input(f"Hello, {player}! Who would you like to take home first? \nRachael, Michelle or Quit? "))
+    first_home(choice1)
 
 def greet() -> None:
-    name: str = str(input(" Welcome message... What's your name? "))
+    name: str = str(input("Welcome to the season finale of the most dramatic season yet of ABC's \"The Bachelor\" where YOU get to help this season's bachelor, Matt James, choose his future wife. You must help Matt as he takes home his final two, Rachael and Michelle, to meet his family... \nWhat's your name? ")) 
+    
     return name
+
+def first_home(choice1: str) -> str:
+    if choice1 == "Rachael":
+        print("you picked Rachael")
+    else:
+        print("you didnt pick rachael")
+
+
+
     
 if __name__ == "__main__":
     main()
